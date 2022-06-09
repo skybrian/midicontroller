9d94753ddc166b73b7c113491b4a7953d50754e4
Brian Slesinsky <brian-git@slesinsky.org>
2022-06-08 19:36:48 -0700

    calculate laps

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,39 @@
 #include "sensor.h"
 
 struct Calculations {
-  float atan2;
+  double theta; // in degrees of phase; 360 degrees = 1 grating
+  double delta; // in degrees
+  double laps;
 };
 
+int laps = -1;
+double prevTheta = nan("");
+
+Calculations calculate(sensor::Reading reading) {
+    Calculations c;
+    c.theta = atan2(reading.b - 300, reading.a - 300) * 360.0 / (2 * PI);
+    double delta = c.theta - prevTheta;
+    prevTheta = c.theta;
+    if (delta < -180.0) {
+      laps++;
+      delta += 360.0;
+    } else if (delta > 180.0) {
+      laps--;
+      delta -= 360.0;
+    }
+    c.delta = delta;
+    c.laps = ((double)laps) + c.theta / 360.0;
+    return c;
+}
+
 void printHeader() {
-  Serial.println("ATan2,A,B,Jitter,A Read Time,B Read Time,Total Read Time");
+  Serial.println("Laps,Delta,Theta,A,B,Jitter,A Read Time,B Read Time,Total Read Time");
 }
 
 void printLine(Calculations c, sensor::Reading r) {
-  Serial.print(c.atan2); Serial.print(", ");
+  Serial.print(c.laps, 4); Serial.print(", ");
+  Serial.print(c.delta); Serial.print(", ");
+  Serial.print(c.theta); Serial.print(", ");
 
   Serial.print(r.a); Serial.print(", ");
   Serial.print(r.b); Serial.print(", ");
@@ -35,9 +59,7 @@ void loop() {
   printHeader();
   while (Serial.dtr()) {
     sensor::Reading reading = sensor::next();
-    Calculations c;
-    c.atan2 = atan2(reading.b - 300, reading.a - 300);
-    printLine(c, reading);
+    printLine(calculate(reading), reading);
   }
 }
 
